10502369fea9dfa7aa0926914464ce41c8b61a2c
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-23 11:26:46 +0530

    creating a toggle functionallity for 3dots

--- src/components/APIinstance/APIinstanceofCard.jsx
+++ src/components/APIinstance/APIinstanceofCard.jsx
@@ -1,3 +1,4 @@
+import React, {useState } from "react";
 import sarthakLogo from "./images/Sarthak Kamra.svg";
 import sarthakCardImg from "./images/Sarthak_Card_Img.svg"
 import SarahWest from "./images/SarahWest.png"
@@ -16,7 +17,8 @@ const apiInstance=[{
     tilte:"✍️ Article",
     heading:"What if famous brands had regular fonts? Meet RegulaBrands!",
     desc:"I've worked in UX for the better part of a decade. From now on, I plan to rei…",
-    views:1.4
+    views:1.4,
+    isClicked:false,
 },{
     cardAvtar:SarahCardImg,
     name:"Sarah West",
@@ -24,7 +26,8 @@ const apiInstance=[{
     tilte:"🔬️ Education",
     heading:"Tax Benefits for Investment under National Pension Scheme launched by Government",
     desc:"I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
-    views:1.4
+    views:1.4,
+    isClicked:false,
 },{
     cardAvtar:RonalCardImg,
     name:"Ronal Jones",
@@ -33,7 +36,8 @@ const apiInstance=[{
     heading:"Finance & Investment Elite Social Mixer @Lujiazui",
     date:"Fri, 12 Oct, 2018",
     location:"Ahmedabad, India",
-    views:1.4
+    views:1.4,
+    isClicked:false,
 },{
     cardAvtar:"",
     name:"Joseph Gray",
@@ -43,11 +47,24 @@ const apiInstance=[{
     company:"💼️ Innovaccer Analytics Private Ltd.",
     date:"Fri, 12 Oct, 2018",
     location:"🛣️ Noida, India",
-    views:1.4
+    views:1.4,
+    is3DClick:false,
 }]
 
-export default function NewApp(){
-    return (<>
+function Component43Dot(props){
+    return(<>
+        <div className="component43Dot" style={{visibility:`${props.isClick?"visible":"hidden"}`}}>
+            <p>Edit</p>
+            <p>Report</p>
+            <p>Option 3</p>
+        </div>
+    </>)
+} 
+
+export default function APIinstanceofCard(){
+    const [isClick, setisClick] = useState(true);
+
+    return (<div style={{marginBottom:"6%"}}>
         {apiInstance.map((obj,i)=>{
             return(
                 <div className="card" key={i}>
@@ -60,9 +77,13 @@ export default function NewApp(){
                     <p>{obj.tilte}</p>
                     <div className="heading">
                         <h3>{obj.heading} </h3>
-                        <span>
+                        <span onClick={()=>{
+                            setisClick(obj.is3DClick?false:true);
+                            obj.is3DClick=isClick
+                        }}>
                             <img src={dots3} alt="3dots"/>
                         </span>
+                        <Component43Dot isClick={obj.is3DClick}/>
                     </div>
                     <p>{obj.desc} </p>
                     <div className="userDetail">
@@ -87,5 +108,5 @@ export default function NewApp(){
             )
         })}
         
-    </>)
+    </div>)
 }
