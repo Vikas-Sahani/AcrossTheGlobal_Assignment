9190a0c34c299298466cda68cb45ddc8334725a6
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-23 19:52:35 +0530

    Registeration component is done

--- src/components/LoginRegister/LoginRegister.jsx
+++ src/components/LoginRegister/LoginRegister.jsx
@@ -1,52 +1,62 @@
 import React from 'react'
 import clear from "../SideComponent/images/clear24px.svg"
 import LogRegImg from "./images/LogRegImg.svg"
+import fbLogo from "./images/fbLogo.svg"
+import gglLogo from "./images/googleLogo.svg"
 
 function LoginRegister() {
   return (
-    <div className='LogRegPg'>
-        <div className='clear'>
-            <img src={clear} alt='clear'/>
-        </div>
-        <div className='coverLogin'>
-            <div className='logReg'>
-                <p>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</p>
+    <>
+        <div className='LogRegPg'>
+            <div className='clear'>
+                <img src={clear} alt='clear'/>
             </div>
-            <div>
-                <div>
-                    <form>
-                        <h1>Create Account</h1>
-                        <input type='text' placeholder='First Name'/>
-                        <input type='text' placeholder='Last Name'/>
-                        <input type='text' placeholder='Email'/>
-                        <input type='text' placeholder='Password'/>
-                        <input type='text' placeholder='Confirm Password'/>
-                        <button>Create Account</button>
-                    </form>
-                    <div className='fb'>
-                        <span className='size'>
-                            <img src={clear} alt='fb'/>
-                        </span>
-                        <p>Sign up with Facebook</p>
-                    </div>
-                    <div className='g'>
-                        <span className='size'>
-                            <img src={clear} alt='g'/>
-                        </span>
-                        <p>Sign up with Google</p>
-                    </div>
+            <div className='coverLogin'>
+                <div className='logReg'>
+                    <p>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</p>
                 </div>
-                <div className='logRegImg'>
-                    <p>Already have an account? Sign In</p>
-                    <div>
-                        <img src={LogRegImg} alt='logRegImg'/>
-                    </div>
-                </div>
-            </div>
+                <div className='createAcc'>
+                    <div className='SignUpform'>
+                        <form>
+                            <h1>Create Account</h1>
+                            <input type='text' placeholder='First Name'/>
+                            <input type='text' placeholder='Last Name'/>
+                            <input type='text' placeholder='Email'/>
+                            <input type='text' placeholder='Password'/>
+                            <input type='text' placeholder='Confirm Password'/>
+                            <button>Create Account</button>
 
+                            <div className='fbgglLogo'>
+                                <span className='size'>
+                                    <img src={fbLogo} alt='fb'/>
+                                </span>
+                                <p>Sign up with Facebook</p>
+                            </div>
+                            <div className='fbgglLogo'>
+                                <span className='size'>
+                                    <img src={gglLogo} alt='g'/>
+                                </span>
+                                <p>Sign up with Google</p>
+                            </div>
+                        </form>
+                        
+                    </div>
+                    <div className='logRegImg'>
+                        <p>Already have an account? <span>Sign In</span></p>
+                        <div>
+                            <img src={LogRegImg} alt='logRegImg'/>
+                        </div>
+                        <p>By signing up, you agree to our Terms & conditions, Privacy policy</p>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
+        <div className='bgLogRegPg'></div>
         
-    </div>
+    
+    </>
   )
 }
 
